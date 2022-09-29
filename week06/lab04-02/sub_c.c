4f672b98c0c612107a4f9bfb6f62250a144d4b58
Greg Benson <benson@usfca.edu>
2022-09-28 23:50:32 -0700

    sub test

---
+++ week06/lab04-02/sub_c.c
@@ -0,0 +1,5 @@
+#include <stdint.h>
+
+uint64_t sub_c(uint64_t a, uint64_t b, uint64_t c) {
+    return (a + b) - c;
+}
---
+++ week06/lab04-02/sub_s.s
@@ -0,0 +1,6 @@
+.global sub_s
+
+sub_s:
+    add a0, a0, a1
+    sub a0, a0, a2
+    ret
